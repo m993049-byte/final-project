e1fb490d95b4287760ea2a174834dd80a29a72b1
marwan <marwan@DESKTOP-DK9FF78>
2025-12-08 14:49:22 +0300

    class done

---
+++ final-project/buyitems.cs
@@ -0,0 +1,9 @@
+﻿namespace final_project
+{
+    public class buyitems
+    {
+        public string Name { get; set; }  
+        public decimal Price { get; set; } 
+        public int Quant { get; set; }    
+    }
+}
